559def6d039b027ec50c37cb42f21e547e19deee
SkyAceHuyquynhtran <vinhlp@tgl-sol.com>
2022-06-24 17:51:43 +0700

    [Admin] add update personality group

---
+++ Contracts/Repositories/IPersonalityGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.DTOs;
+using Entities.RequestFeature;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Contracts.Repositories
+{
+    public interface IPersonalityGroupRepository
+    {
+        Task<List<PerGroupReturn>> GetAllPGroup();
+        Task Update(int pgroup_id, UpdatePGroup info);
+    }
+}
---
+++ Entities/RequestFeature/UpdatePGroup.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Entities.RequestFeature
+{
+    public class UpdatePGroup
+    {
+        [MaxLength(50)]
+        public string PersonalityGroupName { get; set; }
+        public int TestTypeId { get; set; }
+    }
+}
