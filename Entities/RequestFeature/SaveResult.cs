615c60ae10f01f3dec6db7c177e9374f6eb629aa
SkyAceHuyquynhtran <vinhlp@tgl-sol.com>
2022-07-05 17:21:39 +0700

    push blabla

---
+++ Entities/RequestFeature/SaveResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.RequestFeature
+{
+    public class SaveResult
+    {
+        public List<int> ListAnswerId { get; set; }
+    }
+}
