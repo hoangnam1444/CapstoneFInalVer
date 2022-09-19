ca3c7ab1b1be02ab6df71422b6bc01625e58ffe6
SkyAceHuyquynhtran <vinhlp@tgl-sol.com>
2022-09-19 08:51:36 +0700

    edit colleges major

---
+++ Entities/Migrations/20220919014931_edit colleges major.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Entities.Migrations
+{
+    public partial class editcollegesmajor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Colleges_SubjectGroup",
+                table: "Colleges_SubjectGroup");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Colleges_SubjectGroup",
+                table: "Colleges_SubjectGroup",
+                columns: new[] { "CollegesId", "SubjectGroupId", "MajorId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Colleges_SubjectGroup",
+                table: "Colleges_SubjectGroup");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Colleges_SubjectGroup",
+                table: "Colleges_SubjectGroup",
+                columns: new[] { "CollegesId", "SubjectGroupId" });
+        }
+    }
+}
